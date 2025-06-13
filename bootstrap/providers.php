81e8e0ca114537588bbbe0e9e4733f76ba6b37fb
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-06-13 21:24:55 +0100

    add something

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\DoctorPanelProvider::class,
+    App\Providers\Filament\PatientPanelProvider::class,
 ];
