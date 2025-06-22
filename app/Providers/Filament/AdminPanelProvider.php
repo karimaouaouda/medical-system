ec854bcee21f8f871d0c690d526b5b2b28e145dc
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-06-22 21:55:13 +0100

    add simple page to EnsureDoctorApproved.php middleware

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->emailVerification()
+            ->profile(isSimple: false)
             ->login(AdminLogin::class)
             ->databaseNotifications()
             ->colors([
