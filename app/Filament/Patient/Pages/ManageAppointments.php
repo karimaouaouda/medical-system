3ece62a42ae933d518a7f5246cd5205709f901ea
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-06-24 17:27:50 +0100

    add simple page to EnsureDoctorApproved.php middleware

--- app/Filament/Patient/Pages/ManageAppointments.php
+++ app/Filament/Patient/Pages/ManageAppointments.php
@@ -55,7 +55,7 @@ class ManageAppointments extends Page implements HasTable
                     ->using(function (array $data) {
                         $data['patient_id'] = Filament::auth()->id();
                         $data['status'] = AppointmentStatus::Pending;
-                        Appointment::create($data);
+                        return Appointment::create($data);
                     }),
             ]);
     }
