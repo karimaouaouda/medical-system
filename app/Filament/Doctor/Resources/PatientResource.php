8440be9b4fc6467e38c583b8baf282bf90c7446b
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-06-24 23:34:14 +0100

    add simple page to EnsureDoctorApproved.php middleware

--- app/Filament/Doctor/Resources/PatientResource.php
+++ app/Filament/Doctor/Resources/PatientResource.php
@@ -19,7 +19,7 @@ class PatientResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     protected static ?string $label = 'Patient';
 
--- app/Filament/Doctor/Resources/TreatmentResource.php
+++ app/Filament/Doctor/Resources/TreatmentResource.php
@@ -19,7 +19,7 @@ class TreatmentResource extends Resource
 {
     protected static ?string $model = Treatment::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-inbox-stack';
 
     public static function getEloquentQuery(): Builder
     {
